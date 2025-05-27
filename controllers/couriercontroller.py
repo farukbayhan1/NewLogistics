d71838f37854b458a5e52a3cd05535fbcac9ed2f
Faruk Bayhan <b.farukbayhan@gmail.com>
2025-05-27 14:05:12 +0300

    controllers: courier,driver,user

--- controllers/couriercontroller.py
+++ controllers/couriercontroller.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMessageBox, QWidget, QTableWidgetItem, QAbstractItemView, QProgressBar, QApplication
+from PyQt5.QtWidgets import QMessageBox, QWidget, QTableWidget, QTableWidgetItem
 from PyQt5.QtCore import QEvent,QObject
 from services.courierservice import CourierService
 
@@ -9,9 +9,10 @@ class CourierController:
         self.user_role = tab.user_role
         self.service = CourierService()
         self.connect_signal()
+        self.load_couriers()
     
     def connect_signal(self):
-        self.tab.pushButtonCurierAdd.clicked.connect(self.handle_add_courier)
+        self.tab.pushButtonCourierAdd.clicked.connect(self.handle_add_courier)
         
 
     def handle_add_courier(self):
@@ -23,7 +24,7 @@ class CourierController:
 
         # Line Controls
         if not courier_tc_no or not courier_name or not courier_surname or not courier_phone:
-            QMessageBox.warning(self.tab,"Uyarı","Tc No, Ad ve Soyad Zorunludur")
+            QMessageBox.warning(self.tab,"Uyarı","Tc No, Ad, Soyad ve Telefon Numarası Zorunludur")
             return
         elif len(courier_tc_no) != 11 or not courier_tc_no.isdigit():
             QMessageBox.warning(self.tab,"Uyarı","TC No 11 Haneli ve Sadece Rakamlardan Oluşmalıdır")
@@ -53,4 +54,27 @@ class CourierController:
             except Exception as e:
                 QMessageBox.warning(self.tab, "Hata", f"Kurye ekleme işleminde hata oluştu:\n{str(e)}")
 
-        
+    def load_couriers(self):
+        try:
+            courier_list = self.service.get_couriers()
+            self.tab.tableWidgetCouriers.setRowCount(len(courier_list))
+            self.tab.tableWidgetCouriers.setColumnCount(6)
+            self.tab.tableWidgetCouriers.setHorizontalHeaderLabels([
+                "TC No",
+                "Adı",
+                "Soyadı",
+                "Telefon",
+                "Adres",
+                "Ekleyen Kullanıcı"
+            ])
+            for row_index, courier in enumerate(courier_list):
+                self.tab.tableWidgetCouriers.setItem(row_index,0,QTableWidgetItem(courier["courierTcNo"]))
+                self.tab.tableWidgetCouriers.setItem(row_index,1,QTableWidgetItem(courier["courierName"]))
+                self.tab.tableWidgetCouriers.setItem(row_index,2,QTableWidgetItem(courier["courierSurname"]))
+                self.tab.tableWidgetCouriers.setItem(row_index,3,QTableWidgetItem(courier["courierPhone"]))
+                self.tab.tableWidgetCouriers.setItem(row_index,4,QTableWidgetItem(courier["courierAdress"]))
+                self.tab.tableWidgetCouriers.setItem(row_index,5,QTableWidgetItem(courier["userName"]))
+
+        except Exception as e:
+            QMessageBox.warning(self.tab,"Hata","Kurye Bilgileri Getirilirken Hata Oluştu")
+            print(str(e))
